8bf1418477aee7f2c74a3adb14d2dd9f416e9885
Sam <samuel_little93@hotmail.com>
2026-04-27 16:52:23 +1000

    chore: remove bak files, backup folder, fix submodule, remove keys

---
+++ EMRSimulationWebApp/Models/PatientFluidBalanceChartViewModel.cs
@@ -0,0 +1,14 @@
+using EMRSimulation.Domain.Dtos;
+
+namespace EMRSimulationWebApp.Models
+{
+    public class PatientFluidBalanceChartViewModel
+    {
+        public PatientDto? patientDto                              { get; set; }
+        public FluidBalanceChartDto? FluidBalanceChartDto          { get; set; }
+        public IEnumerable<FluidBalanceChartDto>? FluidBalanceChartDtoList { get; set; }
+        public IEnumerable<FluidBalanceChartEntryDto>? FluidBalanceChartEntryDtoList { get; set; }
+        /// <summary>TotalBalance from the patient's most recent chart (0 if none).</summary>
+        public int PreviousDayBalanceAuto                          { get; set; }
+    }
+}
